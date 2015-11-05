52d19c61a8c69acbafd1a8e8cf7809c0712ab8e6
Hekku2 <pekka.savolainen@protacon.com>
2015-11-05 10:34:15 +0000

    Maybe turning eye when communicating with web service

--- hello.py
+++ hello.py
@@ -1,4 +1,15 @@
+#!/usr/bin/python
+
+from ABE_ServoPi import PWM
+import time
+from ABE_helpers import ABEHelpers
 from flask import Flask
+
+bus = ABEHelpers().get_smbus()
+pwm = PWM(bus, 0x40)
+pwm.set_pwn_freq(60)
+pwm.output_enable()
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -7,6 +18,7 @@ def hello():
 
 @app.route("/turn-eye/<int:eye_index>/<int:degree>", methods=['GET'])
 def turn_eye(eye_index, degree):
+  pwm_set_pwm(15, 0, degree)
   return "Kaanto %d %d" % (degree, eye_index)
 
 if __name__ == "__main__":

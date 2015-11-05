f1d0c8a14a773708833e9d5ec8669e3c654aaa26
anhu <antti.humalamaki@protacon.com>
2015-11-05 14:07:25 +0200

    renamed server, added servo test

--- server.py
+++ server.py
@@ -30,6 +30,14 @@ def turn_eye(name, value):
   pwm.set_pwm(index[0], 0, scaled)
   return "Turn %s %d Scaled value: %s" % (name, value, scaled)
 
+@app.route("/servotest/")
+def servotest()
+  for i in [0, 100, 50]
+    for key, value in limits.iteritems()
+      address = value[0]
+      pwm.set_pwm(address, 0, scale_value(key, i))
+      time.sleep(1)
+
 def scale_value(name, value):
   servo_values = limits[name]
   if value < 0:
